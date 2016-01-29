5df5b5fab5a3efba8fa1df93f580e6e6534d4741
veeramarni <marni.srikanth@gmail.com>
2016-01-28 20:06:12 -0500

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'veeramarni:ionic-css',
-  version: '0.0.1',
+  version: '1.2.4',
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
